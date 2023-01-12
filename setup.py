e6c751155477f8154533ffc1b532653d4207156c
Rogerio <rogerioalves.ee@gmail.com>
2023-01-12 10:53:51 -0300

    atualiza setup

--- setup.py
+++ setup.py
@@ -18,12 +18,13 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/rjmalves/idessem",
     packages=find_packages(),
+    package_data={"idessem": ["py.typed"]},
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Development Status :: 1 - Planning"
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     install_requires=requirements
 )
