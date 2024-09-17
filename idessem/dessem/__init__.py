ea38668c305a75edfcdb7ad93ab9aef214b9e857
Rogerio Alves <rogerioalves.ee@gmail.com>
2024-09-17 10:08:33 -0300

    fix imports

--- idessem/dessem/__init__.py
+++ idessem/dessem/__init__.py
@@ -1,36 +1,36 @@
 
 # Inclui os membros
 
-from .avl_altqueda import AvlAltQueda 
-from .avl_desvfpha import AvlDesvFpha
-from .avl_estatfpha import AvlEstatFpha  
-from .avl_fpha1 import AvlFpha1  
-from .avl_fpha2 import AvlFpha2 
-from .avl_fpha3 import AvlFpha3 
-from .des_log_relato import DesLogRelato 
-from .dessemarq import DessemArq  
-from .dessopc import Dessopc  
-from .entdados import Entdados  
-from .hidr import Hidr  
-from .log_matriz import LogMatriz  
-from .operuh import Operuh  
-from .pdo_aval_qmaxusih import PdoAvalQmaxUsih  
-from .pdo_eco_fcfcortes import PdoEcoFcfCortes  
-from .pdo_eco_usih_conj import PdoEcoUsihConj  
-from .pdo_eco_usih_polin import PdoEcoUsihPolin  
-from .pdo_eco_usih import PdoEcoUsih  
-from .pdo_eolica import PdoEolica  
-from .pdo_hidr import PdoHidr  
-from .pdo_inter import PdoInter  
-from .pdo_oper_term import PdoOperTerm  
-from .pdo_oper_tviag_calha import PdoOperTviagCalha  
-from .pdo_oper_uct import PdoOperUct  
-from .pdo_operacao import PdoOperacao  
-from .pdo_reserva import PdoReserva  
-from .pdo_sist import PdoSist  
-from .pdo_term import PdoTerm  
-from .polinjus import Polinjus  
-from .renovaveis import Renovaveis  
-from .termdat import Term  
-from .uch import Uch  
-from .vazaolateral import VazaoLateral  
+from .avl_altqueda import AvlAltQueda  # noqa
+from .avl_desvfpha import AvlDesvFpha  # noqa
+from .avl_estatfpha import AvlEstatFpha    # noqa
+from .avl_fpha1 import AvlFpha1    # noqa
+from .avl_fpha2 import AvlFpha2   # noqa
+from .avl_fpha3 import AvlFpha3   # noqa
+from .des_log_relato import DesLogRelato   # noqa
+from .dessemarq import DessemArq    # noqa
+from .dessopc import Dessopc    # noqa
+from .entdados import Entdados    # noqa
+from .hidr import Hidr    # noqa
+from .log_matriz import LogMatriz    # noqa
+from .operuh import Operuh    # noqa
+from .pdo_aval_qmaxusih import PdoAvalQmaxUsih    # noqa
+from .pdo_eco_fcfcortes import PdoEcoFcfCortes    # noqa
+from .pdo_eco_usih_conj import PdoEcoUsihConj    # noqa
+from .pdo_eco_usih_polin import PdoEcoUsihPolin    # noqa
+from .pdo_eco_usih import PdoEcoUsih    # noqa
+from .pdo_eolica import PdoEolica    # noqa
+from .pdo_hidr import PdoHidr    # noqa
+from .pdo_inter import PdoInter    # noqa
+from .pdo_oper_term import PdoOperTerm    # noqa
+from .pdo_oper_tviag_calha import PdoOperTviagCalha    # noqa
+from .pdo_oper_uct import PdoOperUct    # noqa
+from .pdo_operacao import PdoOperacao    # noqa
+from .pdo_reserva import PdoReserva    # noqa
+from .pdo_sist import PdoSist    # noqa
+from .pdo_term import PdoTerm    # noqa
+from .polinjus import Polinjus    # noqa
+from .renovaveis import Renovaveis    # noqa
+from .termdat import Term    # noqa
+from .uch import Uch    # noqa
+from .vazaolateral import VazaoLateral    # noqa
