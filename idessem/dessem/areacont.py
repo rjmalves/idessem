2a8fdc536962a04361343961d07f176ede43013a
marianasnoel <marianasimoesnoel@gmail.com>
2025-02-21 15:02:14 -0300

    remove métodos deprecados areacont

--- idessem/dessem/areacont.py
+++ idessem/dessem/areacont.py
@@ -1,7 +1,3 @@
-import warnings
-
-# Para compatibilidade - até versão 1.0.0
-from os.path import join
 from typing import Optional, TypeVar
 
 import pandas as pd  # type: ignore
@@ -23,24 +19,6 @@ class Areacont(BlockFile):
 
     def __init__(self, data=...) -> None:
         super().__init__(data)
-
-    @classmethod
-    def le_arquivo(cls, diretorio: str, nome_arquivo="areacont.dat") -> "Areacont":
-        msg = (
-            "O método le_arquivo(diretorio, nome_arquivo) será descontinuado"
-            + " na versão 1.0.0 - use o método read(caminho_arquivo)"
-        )
-        warnings.warn(msg, category=FutureWarning)
-        return cls.read(join(diretorio, nome_arquivo))
-
-    def escreve_arquivo(self, diretorio: str, nome_arquivo="areacont.dat"):
-        msg = (
-            "O método escreve_arquivo(diretorio, nome_arquivo) será"
-            + " descontinuado na versão 1.0.0 -"
-            + " use o método write(caminho_arquivo)"
-        )
-        warnings.warn(msg, category=FutureWarning)
-        self.write(join(diretorio, nome_arquivo))
 
     @property
     def area(self) -> Optional[pd.DataFrame]:
